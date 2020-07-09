e3945fd04214519ca145ea6ea217b85ece0ba806
Jessica Leite <jessica.mleite@gmail.com>
2020-07-09 17:17:26 +0100

    SubmissionsService

--- Source/Services/SubmissionsService.cs
+++ Source/Services/SubmissionsService.cs
@@ -1,27 +1,40 @@
-using System.Collections.Generic;
 using Codenation.Challenge.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Codenation.Challenge.Services
 {
     public class SubmissionService : ISubmissionService
     {
+        private CodenationContext _context;
+
         public SubmissionService(CodenationContext context)
         {
+            _context = context;
         }
 
         public IList<Submission> FindByChallengeIdAndAccelerationId(int challengeId, int accelerationId)
         {
-            throw new System.NotImplementedException();
+            return _context.Submissions
+               .Where(s => s.ChallengeId == challengeId && s.User.Candidates.Any(c => c.AccelerationId == accelerationId))
+               .ToList();
         }
 
         public decimal FindHigherScoreByChallengeId(int challengeId)
         {
-            throw new System.NotImplementedException();
+            return _context.Submissions
+                .Where(s => s.ChallengeId == challengeId)
+                .Select(s => s.Score)
+                .OrderByDescending(s => s)
+                .FirstOrDefault();
         }
 
         public Submission Save(Submission submission)
         {
-            throw new System.NotImplementedException();
+            _context.Add(submission);
+            _context.SaveChanges();
+
+            return submission;
         }
     }
 }
