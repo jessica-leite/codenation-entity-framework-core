c650e88229cf53ba3914aaaec6b33edf88b722c7
Jessica Leite <jessica.mleite@gmail.com>
2020-07-09 17:20:25 +0100

    CompanyService

--- Source/Services/CompanyService.cs
+++ Source/Services/CompanyService.cs
@@ -1,32 +1,56 @@
 using System.Collections.Generic;
+using System.Linq;
 using Codenation.Challenge.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Codenation.Challenge.Services
 {
     public class CompanyService : ICompanyService
     {
+        private CodenationContext _context;
+
         public CompanyService(CodenationContext context)
         {
+            _context = context;
         }
 
         public IList<Company> FindByAccelerationId(int accelerationId)
         {
-            throw new System.NotImplementedException();
+            return _context.Candidates
+                .Include(c => c.Company)
+                .Where(c => c.AccelerationId == accelerationId)
+                .Select(c => c.Company)
+                .ToList();
         }
 
         public Company FindById(int id)
         {
-            throw new System.NotImplementedException();
+            return _context.Companies.Find(id);
         }
 
         public IList<Company> FindByUserId(int userId)
         {
-            throw new System.NotImplementedException();
+            return _context.Candidates
+                .Include(c => c.Company)
+                .Where(c => c.UserId == userId)
+                .Select(c => c.Company)
+                .ToList();
         }
 
         public Company Save(Company company)
         {
-            throw new System.NotImplementedException();
+            if (company.Id == default)
+            {
+                _context.Companies.Add(company);
+            }
+            else
+            {
+                _context.Companies.Update(company);
+            }
+
+            _context.SaveChanges();
+
+            return company;
         }
     }
 }
