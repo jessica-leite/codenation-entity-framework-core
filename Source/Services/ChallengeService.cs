dd95d37863811306fc7db781e2b21579923dc28f
Jessica Leite <jessica.mleite@gmail.com>
2020-07-09 17:19:29 +0100

    ChallengeService

--- Source/Services/ChallengeService.cs
+++ Source/Services/ChallengeService.cs
@@ -1,22 +1,43 @@
 using System.Collections.Generic;
+using System.Linq;
 using Codenation.Challenge.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Codenation.Challenge.Services
 {
     public class ChallengeService : IChallengeService
     {
+        private CodenationContext _context;
+
         public ChallengeService(CodenationContext context)
         {
+            _context = context;
         }
 
         public IList<Models.Challenge> FindByAccelerationIdAndUserId(int accelerationId, int userId)
         {
-            throw new System.NotImplementedException();
+            return _context.Candidates
+                .Where(c => c.AccelerationId == accelerationId && c.UserId == userId)
+                .Include(c => c.Acceleration)
+                    .ThenInclude(a => a.Challenge)
+                .Select(c => c.Acceleration.Challenge)
+                .ToList();
         }
 
         public Models.Challenge Save(Models.Challenge challenge)
         {
-            throw new System.NotImplementedException();
+            if (challenge.Id == default)
+            {
+                _context.Challenges.Add(challenge);
+            }
+            else
+            {
+                _context.Challenges.Update(challenge);
+            }
+
+            _context.SaveChanges();
+
+            return challenge;
         }
     }
 }
